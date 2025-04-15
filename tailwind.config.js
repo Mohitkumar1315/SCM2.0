59a1311fd95466c4c599d8b7ccaafbf127b863ef
Mohitkumar1315 <mvkrampur1315@gmail.com>
2025-04-15 14:08:44 +0530

    Work on Register user form desinging and solve darrk theme mode issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/**/*.{html,js}"], // Adjust as needed
+  content: ["./src/main/resources/templates/**/*.{html,js}"],
+  safelist: [
+    'bg-sky-500',
+    'hover:bg-sky-600',
+    'dark:hover:bg-sky-700',
+    'text-white',
+    'text-gray-900',
+    'rounded-lg',
+    'p-2.5',
+    'w-36'
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
